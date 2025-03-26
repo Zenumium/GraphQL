f08c217ea2cd1d75eb8e9faa4d8e731941d8dfc6
Louvier Valentyn <louvier.valentyn@gmail.com>
2025-03-26 23:39:51 +0100

    fixed ?

--- js/main.js
+++ js/main.js
@@ -432,7 +432,6 @@ function formatXP(xpAmount) {
     return xpAmount >= 1000000 
     ? `: ${(xpAmount / 1000000).toFixed(2)} Mb`  
     : ` Global XP : ${Math.round(xpAmount / 1000)} kb`;
-    
 }
 
 function createAuditRatioSVG(XPup, XPdown) {
